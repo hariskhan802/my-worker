5364192f455280369aea541118bdf6db9bdec904
hariskhan802 <96125873+hariskhan802@users.noreply.github.com>
2026-06-27 11:15:24 +0000

    add worker

---
+++ worker.js
@@ -0,0 +1,21 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    const targetUrl = 'https://api.telegram.org' + url.pathname + url.search;
+
+    const init = {
+      method: request.method,
+      headers: request.headers,
+    };
+
+    if (request.method !== 'GET' && request.method !== 'HEAD') {
+      init.body = await request.clone().arrayBuffer();
+    }
+
+    const response = await fetch(targetUrl, init);
+
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set('Access-Control-Allow-Origin', '*');
+    return newResponse;
+  }
+};
